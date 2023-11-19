dde53720a941acda3181edddcd55598727fdbdbd
chandiran-v <149917528+chandiran-v@users.noreply.github.com>
2023-11-19 12:56:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
+import '/public/index.html'
 import {WeatherApp} from './Components/WeatherApp/WeatherApp.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
