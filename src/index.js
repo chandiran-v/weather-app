2e704a28a3868ec55f886ace9259105b0c570cda
Chandiran <chandiranvc2001@gmail.com>
2023-11-19 12:58:57 +0530

    M

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
-import '/public/index.html'
 import {WeatherApp} from './Components/WeatherApp/WeatherApp.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
